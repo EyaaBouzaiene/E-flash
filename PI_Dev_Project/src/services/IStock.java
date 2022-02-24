5f45cfb592609efe5c7aacf85076cc86b7575c01
khadija <khanfir.khadija@esprit.tn>
2022-02-24 21:19:02 +0100

    Commit1

---
+++ PI_Dev_Project/src/services/IStock.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.Stock;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Khadija
+ */
+public interface IStock<T> {
+
+    public void ajouterS(T s) throws SQLException;
+
+    public List<T> afficherS() throws SQLException;
+
+    public void modifierS() throws SQLException;
+
+    public void supprimerS() throws SQLException;
+    
+   public List<T> rechercheS() throws SQLException;
+   
+   public void Traitement () throws SQLException;
+   
+ 
+
+}
